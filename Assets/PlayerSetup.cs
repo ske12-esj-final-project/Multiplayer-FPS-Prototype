ef404405c86a1135fb29662207402c6805de5710
Raksit Mantanacharu <raksit.m@ku.th>
2018-01-11 11:41:37 +0700

    added networking and disable client's components

---
+++ Assets/PlayerSetup.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+	[SerializeField]
+	Behaviour[] componentsToDisable;
+
+	private Camera sceneCamera;
+
+	void Start() {
+		if (!isLocalPlayer) {
+			for (int i = 0; i < componentsToDisable.Length; i++) {
+				componentsToDisable [i].enabled = false;
+			}
+		} else {
+			sceneCamera = Camera.main;
+			if (sceneCamera != null) {
+				sceneCamera.gameObject.SetActive (false);
+			}
+		}
+	}
+
+	// called when object is destroyed
+	void OnDisable() {
+		if (sceneCamera != null) {
+			sceneCamera.gameObject.SetActive (true);
+		}
+	}
+}
